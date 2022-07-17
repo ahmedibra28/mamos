11aa83458d05b4e0f854087eb044bb57f5b1bc46
unknown <ahmaat19@gmail.com>
2022-07-17 18:48:44 +0300

    adding custom server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const next = require('next')
+const path = require('path')
+
+const port = parseInt(process.env.PORT, 10) || 2020
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  // Serve static files.
+  server.use(express.static(path.join(__dirname, './public')))
+
+  server.use('/_next', express.static(path.join(__dirname, './.next')))
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
