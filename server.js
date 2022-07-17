4cc8d4e117558012073abb9e924ca9033826653a
unknown <ahmaat19@gmail.com>
2022-07-17 19:02:43 +0300

    adding custom server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 const path = require('path')
 
-const port = parseInt(process.env.PORT, 10) || 2020
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
